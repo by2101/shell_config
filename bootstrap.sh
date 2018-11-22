48ea03259274b317da67459c431ba81900097d3a
baiye <ye.bai@nlpr.ia.ac.cn>
2018-11-22 21:06:02 +0800

    clone z

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 CONFIG_PATH=$(cd "$(dirname "$0")"; pwd)
 
-
 echo source $CONFIG_PATH/init.sh >> ~/.bashrc
+
+echo "Installing plug: z"
+cd plugs
+https://github.com/rupa/z.git
+cd $CONFIG_PATH
 
 echo "Shell configuration is initialized."
 
