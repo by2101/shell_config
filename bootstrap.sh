c779e7fe401a4282ea2381a21f683b01c58404b7
baiye <ye.bai@nlpr.ia.ac.cn>
2018-11-22 21:13:22 +0800

    mkdir plugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,11 @@ CONFIG_PATH=$(cd "$(dirname "$0")"; pwd)
 
 echo source $CONFIG_PATH/init.sh >> ~/.bashrc
 
+[ ! -d $CONFIG_PATH/plugs ] && mkdir -p $CONFIG_PATH/plugs
+ 
 echo "Installing plug: z"
 cd plugs
-https://github.com/rupa/z.git
+git clone https://github.com/rupa/z.git
 cd $CONFIG_PATH
 
 echo "Shell configuration is initialized."
